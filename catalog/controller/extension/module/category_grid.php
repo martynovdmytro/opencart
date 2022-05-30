160eac42b443597a3d2d7ea396ac8a451aa309e6
froze <martynovdmytriy@gmail.com>
2022-05-30 12:02:34 +0300

    catalog module category_grid including logic has been added and connected to the general system. html code to display category data should be created in category_frid.twig

---
+++ catalog/controller/extension/module/category_grid.php
@@ -0,0 +1,49 @@
+<?php
+class ControllerExtensionModuleCategoryGrid extends Controller{
+    public function index($setting) {
+
+        $this->load->language('extension/module/category_grid');
+
+        $this->load->language('catalog/category');
+
+        $this->load->model('catalog/category');
+
+        $this->load->model('tool/image');
+
+        $data['categories'] = array();
+
+        if (!$setting['limit']) {
+            $setting['limit'] = 4;
+        }
+
+        if (!empty($setting['category'])) {
+            $categories = array_slice($setting['category'], 0, (int)$setting['limit']);
+
+            foreach ($categories as $category_id) {
+                $category_info = $this->model_catalog_category->getCategory($category_id);
+
+                if ($category_info) {
+                    if ($category_info['image']) {
+                        $image = $this->model_tool_image->resize($category_info['image'], $setting['width'], $setting['height']);
+                    } else {
+                        $image = $this->model_tool_image->resize('placeholder.png', $setting['width'], $setting['height']);
+                    }
+
+                    $data['categories'][] = array
+                    (
+                        'category_id' => $category_info['category_id'],
+                        'thumb'       => $image,
+                        'name'        => $category_info['name'],
+                        'href'        => $this->url->link('product/product', 'category_id=' . $category_info['category_id'])
+                    );
+
+                }
+
+            }
+
+        }
+
+        return $this->load->view('extension/module/category_grid');
+
+    }
+}
