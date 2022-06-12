ede4c1ef2a14d50a1716586424e8e996d7a5f0ac
0nefroze <martynovdmytriy@gmail.com>
2022-06-12 07:24:39 +0300

    master

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -76,6 +76,7 @@ class ControllerCommonHeader extends Controller {
 		$data['search'] = $this->load->controller('common/search');
 		$data['cart'] = $this->load->controller('common/cart');
 		$data['menu'] = $this->load->controller('common/menu');
+        
 
 		return $this->load->view('common/header', $data);
 	}
